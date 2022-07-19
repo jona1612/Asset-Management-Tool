7727d5ec3468f3850dc0a6eafd4d867611f370a4
Jonathan <jonathan16official@gmail.com>
2022-07-20 02:23:43 +0530

    New

---
+++ src/Pages/List/List.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./List.scss"
+import Sidebar from "../../Components/Sidebar/Sidebar"
+import Navbar from "../../Components/Navbar/Navbar"
+import Datatable from '../../Components/Datatable/Datatable'
+
+
+const List = () => {
+  return (
+    <div className='list'>
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  )
+}
+
+export default List;
